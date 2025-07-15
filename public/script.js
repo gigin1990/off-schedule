30c86ec2eff4a0ce22516c0cbbae9105df3a9373
gigin1990 <karyawanmultiverse@gmail.com>
2025-07-15 14:47:12 +0700

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function () {
   const calendarEl = document.getElementById("calendar");
   const calendar = new FullCalendar.Calendar(calendarEl, {
@@ -23,23 +22,15 @@ document.addEventListener("DOMContentLoaded", function () {
     eventList.forEach((event) => calendar.addEvent(event));
 
     fetch("https://script.google.com/macros/s/AKfycbw11gVktegPQnCqvlVlSn3zMMzBqez1yUUkaK7kq5l3lw9EjrA-4JDVO0UD9G9-RoDO3Q/exec", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    name: name,
-    dates: dates.split(",").map(d => d.trim())  // pastikan bentuk array
-  }),
-})
-.then(response => response.text())
-.then(result => {
-  console.log("Sukses kirim:", result);
-  alert("Data berhasil dikirim!");
-  document.getElementById("offForm").reset();
-})
-.catch(error => {
-  console.error("Gagal kirim:", error);
-  alert("Gagal mengirim data");
-});
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, dates }),
+    });
+
+    document.getElementById("offForm").reset();
+    alert("Data berhasil dikirim!");
+  });
 });
